501a96f06b0a25e33fc952c205af511da20ccc84
Badsha Hossain <badshab454@gmail.com>
2023-01-31 18:36:31 +0600

    remove h2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>My name is {name}</h2>
         <p>
         React Course In Bangladesh.
         </p>
