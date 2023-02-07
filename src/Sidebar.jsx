d0f144f6059eefcd41a7b2af5749ed9cfd168b77
Badsha Hossain <badshab454@gmail.com>
2023-02-08 00:52:02 +0600

    default  component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,4 +2,10 @@ function Sidebar () {
     return <h1>This is The Sidebar Component.</h1>
 }
 
-export default Sidebar;
+const Footer = () => {
+    return (
+        <h1>Footer</h1>
+    )
+}
+
+export default Footer;
