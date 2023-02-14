5e20eb55bb0dce81e6f9acac1a29bc674e9ed4f5
Badsha Hossain <badshab454@gmail.com>
2023-02-14 21:00:31 +0600

    movie website

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import Student from './components/Student';
 
 function App() {
   return (
     <div className="App">
-      <Student 
-        name="Badsha Hossain" 
-        age={26} 
-      />
-      <Student 
-        name="Masud Rana" 
-        age={28} 
-      />
-      <Student 
-        name="Minhajur Rahman" 
-        age="bold"
-      />
+      <h2>Hello World!</h2>
     </div>
   );
 }
