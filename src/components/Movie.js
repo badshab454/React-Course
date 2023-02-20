5dc75900fbde6a5bec18cb30ae902f2cc6d3268b
Badsha Hossain <badshab454@gmail.com>
2023-02-21 00:57:20 +0600

    movie margin

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,7 +3,7 @@ const Movie = () => {
     <div className="movie">
         <img src="https://assets.reedpopcdn.com/Annotation-2022-09-20-135505.png/BROK/resize/1200x1200%3E/format/jpg/quality/70/Annotation-2022-09-20-135505.png" alt="Iron Man 1" />
         <p>Iron Man 1</p>
-        <p>Year: 2008</p>
+        <p>Year: 1998</p>
     </div>
   )
 }
