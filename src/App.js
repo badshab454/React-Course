6b47bb8bc63bc1579fa9556be92dbdb775933834
Badsha Hossain <badshab454@gmail.com>
2023-02-23 00:43:17 +0600

    live server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
-import Header from './components/Header'; 
-import Movie from './components/Movie';
+import "./App.css";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
 
 function App() {
+  let login = false;
+
+  if (login == true) {
+    return <Header />;
+  } else {
+    return <h1>Hello World!</h1>;
+  }
+
   return (
     <div className="App">
       <Header />
@@ -13,6 +21,7 @@ function App() {
         <Movie />
         <Movie />
         <Movie />
+        <h1>Hello World!</h1>
       </div>
     </div>
   );
