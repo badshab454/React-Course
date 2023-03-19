ee521a2a336762fe713872c46b03adc459c5cfbf
Badsha Hossain <badshab454@gmail.com>
2023-03-20 00:55:13 +0600

    runway clothing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import "./App.css";
 import Header from "./components/Header";
-import Movie from "./components/Movie";
 
 function App() {
-  let login = false;
-
-  if (login == true) {
-    return <Header />;
-  } else {
-    return <h1>Hello World!</h1>;
-  }
-
-  return (
-    <div className="App">
-      <Header />
-      <div className="main">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <h1>Hello World!</h1>
-      </div>
-    </div>
-  );
+  return <h1>Hello Coder</h1>;
 }
 
 export default App;
