144abbd25b5bafcd541c47168051f676374b4b97
Badsha Hossain <badshab454@gmail.com>
2023-03-21 22:12:18 +0600

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Home from "./Components/Home";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <h1>Hello Coder</h1>;
+  return (
+    <div className="App">
+      <Home />
+      <button className="btn btn-primary text-center">Try me</button>
+      <div className="container">
+        <div className="row">
+          <h2 className="col-md-6">Hello</h2>
+          <h2 className="col-md-6">Hello</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
